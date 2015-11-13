97420d945f78e5546efd2f8be3a1b12ef059fbdc
Silvia C <silviachen917@gmail.com>
2015-11-13 14:11:47 -0500

    some changes

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -7,10 +7,7 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state) 
         link: function (scope) {
 
             scope.items = [
-                { label: 'Home', state: 'home' },
-                { label: 'About', state: 'about' },
-                { label: 'Documentation', state: 'docs' },
-                { label: 'Members Only', state: 'membersOnly', auth: true }
+                { label: 'Home', state: 'home' }
             ];
 
             scope.user = null;
@@ -24,6 +21,10 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state) 
                    $state.go('home');
                 });
             };
+
+            scope.goHome = function() {
+                $state.go('home');
+            }
 
             var setUser = function () {
                 AuthService.getLoggedInUser().then(function (user) {
